8bcfd8da598a4cafd8bbfa25677373b52fb2dcd5
Luan Chrystian Galan <luanchrystian2@gmail.com>
2020-08-20 19:34:32 -0300

    Auto-generated commit

---
+++ assets/cards.ts
@@ -0,0 +1,210 @@
+export const cards = [
+    {
+      "suit": "C",
+      "value": 2
+    },
+    {
+      "suit": "C",
+      "value": 3
+    },
+    {
+      "suit": "C",
+      "value": 4
+    },
+    {
+      "suit": "C",
+      "value": 5
+    },
+    {
+      "suit": "C",
+      "value": 6
+    },
+    {
+      "suit": "C",
+      "value": 7
+    },
+    {
+      "suit": "C",
+      "value": 8
+    },
+    {
+      "suit": "C",
+      "value": 9
+    },
+    {
+      "suit": "C",
+      "value": 10
+    },
+    {
+      "suit": "C",
+      "value": "J"
+    },
+    {
+      "suit": "C",
+      "value": "Q"
+    },
+    {
+      "suit": "C",
+      "value": "K"
+    },
+    {
+      "suit": "C",
+      "value": "A"
+    },
+    {
+      "suit": "O",
+      "value": 2
+    },
+    {
+      "suit": "O",
+      "value": 3
+    },
+    {
+      "suit": "O",
+      "value": 4
+    },
+    {
+      "suit": "O",
+      "value": 5
+    },
+    {
+      "suit": "O",
+      "value": 6
+    },
+    {
+      "suit": "O",
+      "value": 7
+    },
+    {
+      "suit": "O",
+      "value": 8
+    },
+    {
+      "suit": "O",
+      "value": 9
+    },
+    {
+      "suit": "O",
+      "value": 10
+    },
+    {
+      "suit": "O",
+      "value": "J"
+    },
+    {
+      "suit": "O",
+      "value": "Q"
+    },
+    {
+      "suit": "O",
+      "value": "K"
+    },
+    {
+      "suit": "O",
+      "value": "A"
+    },
+    {
+      "suit": "C",
+      "value": 2
+    },
+    {
+      "suit": "C",
+      "value": 3
+    },
+    {
+      "suit": "C",
+      "value": 4
+    },
+    {
+      "suit": "C",
+      "value": 5
+    },
+    {
+      "suit": "C",
+      "value": 6
+    },
+    {
+      "suit": "C",
+      "value": 7
+    },
+    {
+      "suit": "C",
+      "value": 8
+    },
+    {
+      "suit": "C",
+      "value": 9
+    },
+    {
+      "suit": "C",
+      "value": 10
+    },
+    {
+      "suit": "C",
+      "value": "J"
+    },
+    {
+      "suit": "C",
+      "value": "Q"
+    },
+    {
+      "suit": "C",
+      "value": "K"
+    },
+    {
+      "suit": "C",
+      "value": "A"
+    },
+    {
+      "suit": "E",
+      "value": 2
+    },
+    {
+      "suit": "E",
+      "value": 3
+    },
+    {
+      "suit": "E",
+      "value": 4
+    },
+    {
+      "suit": "E",
+      "value": 5
+    },
+    {
+      "suit": "E",
+      "value": 6
+    },
+    {
+      "suit": "E",
+      "value": 7
+    },
+    {
+      "suit": "E",
+      "value": 8
+    },
+    {
+      "suit": "E",
+      "value": 9
+    },
+    {
+      "suit": "E",
+      "value": 10
+    },
+    {
+      "suit": "E",
+      "value": "J"
+    },
+    {
+      "suit": "E",
+      "value": "Q"
+    },
+    {
+      "suit": "E",
+      "value": "K"
+    },
+    {
+      "suit": "E",
+      "value": "A"
+    }
+  ]
